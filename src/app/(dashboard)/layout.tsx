d521e825a06be6b416a54f9afe2bc612c0d86941
Pranjal6955 <negipranjal555@gmail.com>
2025-07-28 16:40:27 +0530

    Implemented Prettier

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+
+type LayoutProps = {
+    children:ReactNode;
+};
+const Layout = ({ children }: LayoutProps) => {
+    return <>{children}</>
+
+}
+
+export default Layout;
+
